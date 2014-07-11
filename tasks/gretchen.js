022ebde7e5b699023ce09fc64fd4aa144d1d3625
Carl Sutherland <carl@humblesoftware.com>
2014-07-11 10:35:26 -0700

    Renamed fetch-cli to gretchen.

--- tasks/gretchen.js
+++ tasks/gretchen.js
@@ -13,7 +13,7 @@ module.exports = function(grunt) {
   /**
    * Build the mocks for our test runner.
    */
-  var bundle = require('fetch-cli/lib/bundle');
+  var bundle = require('gretchen/lib/bundle');
 
   grunt.registerMultiTask('gretchen', 'Making grunt make fetch happen.', function() {
     var done = this.async();
